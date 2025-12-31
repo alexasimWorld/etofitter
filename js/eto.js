ed227cd3a4c7427945dbcb64dcfdc3cf8b20fc19
alexasimWorld <alexandros.asimakopoulos@outlook.com>
2025-12-31 12:51:07 +0200

    Add files via upload

---
+++ js/eto.js
@@ -0,0 +1,54 @@
+window.ETO_CREW = [
+  {
+    id: "eto-001",
+    type: "ETO",
+    name: "J. Doum1",
+    rank: "Electro-Technical Officer",
+    nationality: "GR",
+    email: "eto.doumen@doumen.local",
+    phone: "+0000000",
+    handler: "E-12",
+    status: "status2",
+    photoDataUrl: "",
+    attachments: []
+  },
+  {
+    id: "eto-002",
+    type: "ETO",
+    name: "J. Doum2",
+    rank: "ETO",
+    nationality: "PH",
+    email: "eto.doumen2@doumen.local",
+    phone: "+00000000",
+    handler: "E-14",
+    status: "status1",
+    photoDataUrl: "",
+    attachments: []
+  },
+    {
+    id: "eto-003",
+    type: "ETO",
+    name: "J. Doum3",
+    rank: "ETO",
+    nationality: "PH",
+    email: "eto.doumen3@doumen.local",
+    phone: "+00000000",
+    handler: "E-14",
+    status: "status1",
+    photoDataUrl: "",
+    attachments: []
+  },
+    {
+    id: "eto-004",
+    type: "ETO",
+    name: "J. Doum4",
+    rank: "ETO",
+    nationality: "PH",
+    email: "eto.doumen4@doumen.local",
+    phone: "+00000000",
+    handler: "E-14",
+    status: "status1",
+    photoDataUrl: "",
+    attachments: []
+  }
+];
